733542e1955c0ded03b452e0f03a3eec1cf0fe36
Darwin Galindez <darwin.galindez@ceiba.com.co>
2022-02-15 14:25:20 -0500

    Se agregaron test para nueva entidad PagosAdministracion

---
+++ microservicio/dominio/src/test/java/com/ceiba/pagos/administracion/entidad/PagosAdministracionTest.java
@@ -0,0 +1,67 @@
+package com.ceiba.pagos.administracion.entidad;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
+import com.ceiba.pagos.administracion.modelo.entidad.PagosAdministracion;
+import com.ceiba.pagos.administracion.servicio.testdatabuilder.PagosAdministracionTestDataBuilder;
+import com.ceiba.usuario.modelo.entidad.Usuario;
+import com.ceiba.usuario.servicio.testdatabuilder.UsuarioTestDataBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PagosAdministracionTest {
+
+    @Test
+    @DisplayName("Deberia crear correctamente el pago de administración")
+    void deberiaCrearCorrectamenteElUsusuario() {
+        // arrange
+        LocalDateTime fechaCreacion = LocalDateTime.now();
+        //act
+        PagosAdministracion pagosAdministracion = new PagosAdministracionTestDataBuilder().conFechaCreacion(fechaCreacion).conId(1L).build();
+        //assert
+        assertEquals(1, pagosAdministracion.getId());
+        assertEquals("AQ17", pagosAdministracion.getCodigoInmueble());
+        assertEquals(1324549876, pagosAdministracion.getNumeroIdentificacionPersona());
+        assertEquals(true, pagosAdministracion.getPagoTotal());
+    }
+
+    @Test
+    void deberiaFallarSinCodigoInmueble() {
+
+        //Arrange
+        PagosAdministracionTestDataBuilder pagosAdministracionTestDataBuilder = new PagosAdministracionTestDataBuilder().conCodigoInmueble(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    pagosAdministracionTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar el código del inmueble");
+    }
+
+    @Test
+    void deberiaFallarSinNumeroIdentificacionPersona() {
+
+        //Arrange
+        PagosAdministracionTestDataBuilder pagosAdministracionTestDataBuilder = new PagosAdministracionTestDataBuilder().conNumeroIdentificacionPersona(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    pagosAdministracionTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar el número de identificación de la persona");
+    }
+
+    @Test
+    void deberiaFallarSinValorPagado() {
+
+        //Arrange
+        PagosAdministracionTestDataBuilder pagosAdministracionTestDataBuilder = new PagosAdministracionTestDataBuilder().conValorPagado(null).conId(1L);
+        //act-assert
+        BasePrueba.assertThrows(() -> {
+                    pagosAdministracionTestDataBuilder.build();
+                },
+                ExcepcionValorObligatorio.class, "Se debe ingresar el valor a pagar");
+    }
+}
---
+++ microservicio/dominio/src/test/java/com/ceiba/pagos/administracion/servicio/testdatabuilder/PagosAdministracionTestDataBuilder.java
@@ -0,0 +1,80 @@
+package com.ceiba.pagos.administracion.servicio.testdatabuilder;
+
+import com.ceiba.pagos.administracion.modelo.entidad.PagosAdministracion;
+
+import java.time.LocalDateTime;
+
+public class PagosAdministracionTestDataBuilder {
+
+    private Long id;
+    private String codigoInmueble;
+    private Integer numeroIdentificacionPersona;
+    private Integer valorPagado;
+    private Integer porcentajeInteres;
+    private Integer valorPagadoInteres;
+    private Boolean pagoTotal;
+    private LocalDateTime fechaCreacion;
+
+    public PagosAdministracionTestDataBuilder() {
+        codigoInmueble = "AQ17";
+        numeroIdentificacionPersona = 1324549876;
+        valorPagado = 300000;
+        porcentajeInteres = 0;
+        valorPagadoInteres = 0;
+        pagoTotal = true;
+        fechaCreacion = LocalDateTime.now();
+    }
+
+    public PagosAdministracionTestDataBuilder conId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conCodigoInmueble(String codigoInmueble) {
+        this.codigoInmueble = codigoInmueble;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conNumeroIdentificacionPersona(Integer numeroIdentificacionPersona) {
+        this.numeroIdentificacionPersona = numeroIdentificacionPersona;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conValorPagado(Integer valorPagado) {
+        this.valorPagado = valorPagado;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conPorcentajeInteres(Integer porcentajeInteres) {
+        this.porcentajeInteres = porcentajeInteres;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conValorPagadoInteres(Integer valorPagadoInteres) {
+        this.valorPagadoInteres = valorPagadoInteres;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conPagoTotal(Boolean pagoTotal) {
+        this.pagoTotal = pagoTotal;
+        return this;
+    }
+
+    public PagosAdministracionTestDataBuilder conFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+        return this;
+    }
+
+    public PagosAdministracion build() {
+        return new PagosAdministracion (
+                id,
+                codigoInmueble,
+                numeroIdentificacionPersona,
+                valorPagado,
+                porcentajeInteres,
+                valorPagadoInteres,
+                pagoTotal,
+                fechaCreacion
+        );
+    }
+}
