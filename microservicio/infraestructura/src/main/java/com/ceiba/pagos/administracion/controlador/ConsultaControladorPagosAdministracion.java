b500237d5bbe15742b472ff39bcb73743a81c2dd
Darwin Galindez <darwin.galindez@ceiba.com.co>
2022-02-23 18:39:40 -0500

    Se elimino logger

--- microservicio/infraestructura/src/main/java/com/ceiba/pagos/administracion/controlador/ConsultaControladorPagosAdministracion.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/pagos/administracion/controlador/ConsultaControladorPagosAdministracion.java
@@ -1,34 +1,40 @@
 package com.ceiba.pagos.administracion.controlador;
 
+import com.ceiba.pagos.administracion.consulta.ManejadorListarPagosAdministracion;
 import com.ceiba.pagos.administracion.consulta.ManejadorTotalPagadoPagosAdministracion;
 import com.ceiba.pagos.administracion.modelo.dto.DtoConsultarSaldoPagosAdministracion;
+import com.ceiba.pagos.administracion.modelo.dto.DtoPagosAdministracion;
 import com.ceiba.pagos.administracion.modelo.dto.DtoSaldoPagosAdministracion;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pagos")
 @Api(tags = {"Controlador para consultas pagos administracion"})
 public class ConsultaControladorPagosAdministracion {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ManejadorListarPagosAdministracion manejadorListarPagosAdministracion;
     private final ManejadorTotalPagadoPagosAdministracion manejadorTotalPagadoPagosAdministracion;
 
     @Autowired
-    public ConsultaControladorPagosAdministracion(ManejadorTotalPagadoPagosAdministracion manejadorTotalPagadoPagosAdministracion) {
+    public ConsultaControladorPagosAdministracion(ManejadorTotalPagadoPagosAdministracion manejadorTotalPagadoPagosAdministracion, ManejadorListarPagosAdministracion manejadorListarPagosAdministracion) {
         this.manejadorTotalPagadoPagosAdministracion = manejadorTotalPagadoPagosAdministracion;
+        this.manejadorListarPagosAdministracion = manejadorListarPagosAdministracion;
     }
 
     @PostMapping("/consultar-saldo")
     @ApiOperation("Consultar el saldo de un inmueble por codigo y mes")
     public DtoSaldoPagosAdministracion consultarSaldo(@RequestBody DtoConsultarSaldoPagosAdministracion consultarSaldoPagosAdministracion) {
         return manejadorTotalPagadoPagosAdministracion.ejecutar(consultarSaldoPagosAdministracion);
+    }
+
+    @GetMapping("/listar")
+    @ApiOperation("Listar todos los pagos realizados")
+    public List<DtoPagosAdministracion> listar() {
+        return manejadorListarPagosAdministracion.ejecutar();
     }
 }
